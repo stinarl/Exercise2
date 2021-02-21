78dfd1d3b74314aa9f4d16bc06aae6dbc3867326
stinarl <69670955+stinarl@users.noreply.github.com>
2021-02-21 23:33:57 +0100

    Finished foo

--- 2 - shared variable/c/foo.c
+++ 2 - shared variable/c/foo.c
@@ -1,35 +1,50 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <mutex>
+
+std::mutex mtx;
 
 int i = 0;
 
-// Note the return type: void*
-void* incrementingThreadFunction(){
-    for (int j = 0; j < 1000000; j++) {
-	// TODO: sync access to i
-	i++;
+
+pthread_mutex_t lock;
+/**
+ * Mutex is thread-otiented and semaphores are token oriented. Since we in this case is
+ * working on two particular threads it seams reasonable to use mutex. Sem_t might be
+ * prefered when we use interrupt handlers.
+ */
+void *incrementingThreadFunction(void* num) {
+    for (int j = 0; j < 1000099; j++) {
+        mtx.lock();                // Acquire lock
+        (*(int*)num)++;              // Critical work
+        mtx.unlock();           // Release lock
     }
+
     return NULL;
 }
 
-void* decrementingThreadFunction(){
+void *decrementingThreadFunction(void* num) {
     for (int j = 0; j < 1000000; j++) {
-	// TODO: sync access to i
-	i--;
+        mtx.lock();;
+        (*(int*)num)--;
+        mtx.unlock();
     }
+
     return NULL;
 }
 
 
-int main(){
+int main() {
+    pthread_mutex_init(&lock, NULL);
+
     pthread_t incrementingThread, decrementingThread;
-    
-    pthread_create(&incrementingThread, NULL, incrementingThreadFunction, NULL);
-    pthread_create(&decrementingThread, NULL, decrementingThreadFunction, NULL);
-    
+
+    pthread_create(&incrementingThread, NULL, incrementingThreadFunction, &i);
+    pthread_create(&decrementingThread, NULL, decrementingThreadFunction, &i);
+
     pthread_join(incrementingThread, NULL);
     pthread_join(decrementingThread, NULL);
-    
+
     printf("The magic number is: %d\n", i);
+
     return 0;
-}
